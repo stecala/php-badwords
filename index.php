1b29a3de1ca9f343a59214e632aa9d5b83626f5f
stecala <ste.cala123@gmail.com>
2022-08-22 15:16:42 +0200

    ⭐Feat (bonus) : Check if $censura is empty and contained in $text

--- index.php
+++ index.php
@@ -10,15 +10,24 @@
     <?php
         $text = 'Ciao, Sono Stefano e studio per diventare web developer. Ciao ciao';
         
-        echo $text .'. Questa frase e\' lunga '. strlen($text). ' caratteri';
-        
+        echo $text .'. Questa frase e\' lunga '. strlen($text). ' caratteri <br>';
+        error_reporting(E_ALL & ~E_NOTICE);
         $censura = $_GET['censura'];
+        
+        if(is_null($censura)){
+            $fraseCensurata = 'Non hai dichiarato la censura';
+            echo $fraseCensurata;
+        }
+        else{
+            if(strpos($text, $censura) !== false){
+                $fraseCensurata = str_replace($censura , '***' , $text);
+                echo $fraseCensurata .' Questa frase e\' lunga '. strlen($fraseCensurata). ' caratteri';
+            }
+            else{
+                echo 'La censura che hai inserito non e\' contenuta nella frase';
+            }
+        }
     ?>
-    <br>
-    <?php
-        $fraseCensurata = str_replace($censura , '***' , $text);
 
-        echo $fraseCensurata .' Questa frase e\' lunga '. strlen($fraseCensurata). ' caratteri'
-    ?>
 </body>
 </html>
